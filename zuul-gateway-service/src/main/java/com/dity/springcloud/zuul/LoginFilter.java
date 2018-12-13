fb9b85e39e7dcc4de2242b2651d741362a0f292b
yuhang <649411629@qq.com>
2018-12-13 21:15:21 +0800

    zuul网关

--- zuul-gateway-service/src/main/java/com/dity/springcloud/zuul/LoginFilter.java
+++ zuul-gateway-service/src/main/java/com/dity/springcloud/zuul/LoginFilter.java
@@ -16,21 +16,40 @@ import java.io.IOException;
 @Component
 public class LoginFilter extends ZuulFilter {
 
+    // //枚举值：pre, routing, post, error
+    /**
+     * filterType：表示过滤类型。
+     * pre表示路由之前， routing表示路由当中， post表示路由之后， error表示路由发生错误。
+     */
     @Override
     public String filterType() {
         return "pre";
     }
 
+    /**
+     * filterOrder： 执行时序， 值是0,1,2....N等自然数。 0的优先级最高，即最先执行。
+     */
     @Override
     public int filterOrder() {
         return 0;
     }
 
+    /**
+     * shouldFilter： 是否需要执行run函数。
+     * 即该过滤器是否生效
+     * false 不生效
+     * true 生效
+     */
     @Override
     public boolean shouldFilter() {
         return true;
     }
 
+    /**
+     * 具体过滤的方法
+     * @return
+     * @throws ZuulException
+     */
     @Override
     public Object run() throws ZuulException {
         RequestContext ctx = RequestContext.getCurrentContext();
