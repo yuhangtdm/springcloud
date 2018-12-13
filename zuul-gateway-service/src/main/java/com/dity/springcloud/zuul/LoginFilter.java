174e1c5d9ed4f07ddcc7fef06e94713846c61078
yuhang <649411629@qq.com>
2018-12-13 20:39:38 +0800

    zuul网关

---
+++ zuul-gateway-service/src/main/java/com/dity/springcloud/zuul/LoginFilter.java
@@ -0,0 +1,53 @@
+package com.dity.springcloud.zuul;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+/**
+ * @author:yuhang
+ * @Date:2018/12/13
+ */
+@Component
+public class LoginFilter extends ZuulFilter {
+
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+        String token = request.getParameter("token");
+        if (StringUtils.isEmpty(token)){
+            // 不转发
+            ctx.setSendZuulResponse(false);
+            ctx.setResponseStatusCode(401);
+            try {
+                ctx.getResponse().getWriter().write("token is empty");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+        return null;
+    }
+}
